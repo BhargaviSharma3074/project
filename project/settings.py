07dd69febe908e79b81ad21ac1d984b99f069e9b
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-11-14 13:24:02 +0530

    update

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,13 +25,8 @@ SECRET_KEY = 'django-insecure-b+svqcnag#127bz6w$e6xyvsdifi04r4c%f_zv&oxyjr^+4+7g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 # Application definition
 
